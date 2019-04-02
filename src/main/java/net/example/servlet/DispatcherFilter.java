703a413a02a84a98bdaeaef5d1e5c70d4e5e9e3e
eugene <eugenicon@gmail.com>
2019-04-03 00:58:20 +0300

    added request dispatching filter

---
+++ src/main/java/net/example/servlet/DispatcherFilter.java
@@ -0,0 +1,35 @@
+package net.example.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@WebFilter(urlPatterns = "*", initParams = {
+        @WebInitParam(name = "dispatcherUrl", value = "/view"),
+        @WebInitParam(name = "permittedUrls", value = "/WEB-INF/,/static/")
+})
+public class DispatcherFilter implements Filter {
+    private String dispatcherUrl;
+    private List<String> permittedUrls;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        dispatcherUrl = filterConfig.getInitParameter("dispatcherUrl");
+        permittedUrls = Arrays.asList(filterConfig.getInitParameter("permittedUrls").split(","));
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String path = httpRequest.getRequestURI().substring( httpRequest.getContextPath().length());
+        if (permittedUrls.stream().anyMatch(path::startsWith)) {
+            chain.doFilter(request, response);
+        } else {
+            request.getRequestDispatcher(dispatcherUrl + path).forward(request, response);
+        }
+    }
+}
